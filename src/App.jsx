c5bbb9120d10105ae97cdedb130512f8af2c6911
hinimdoumorsia <hinimdoumorsia@gmail.com>
2026-05-09 20:04:25 +0100

    gestion des conflits

--- src/App.jsx
+++ src/App.jsx
@@ -12,22 +12,6 @@ import Training from "./pages/Training";
 import DonneesPage from './pages/Donnees/index';
 import ModelsPage from './pages/Models';
 import AgentsPage from "./pages/Agents/index";
-<<<<<<< HEAD
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/predictions" replace />} />
-        <Route path="/predictions" element={<Predictions />} />
-        <Route path="/entrainement" element={<Training />} /> {/* Changé ici */}
-        <Route path="*" element={<Navigate to="/predictions" replace />} />
-        <Route path="/donnees" element={<DonneesPage />} />
-        <Route path="/models" element={<ModelsPage />} />
-        <Route path="/agents" element={<AgentsPage />} />
-      </Routes>
-    </BrowserRouter>
-=======
 import OutilsPage from './pages/Outils';
 import ParametresPage from './pages/Parametres';
 import MaintenancePage from './pages/Maintenance';
@@ -108,7 +92,6 @@ const App = () => {
     <AuthProvider>
       <ProtectedApp />
     </AuthProvider>
->>>>>>> djeriV2
   );
 };
 
